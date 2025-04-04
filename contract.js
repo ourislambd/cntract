e28e7cc205f956cc3f0dd8538156e334cade7195
OURISLAMBD <65226329+ourislambd@users.noreply.github.com>
2025-04-04 21:52:33 +0600

    Update contract.js

--- contract.js
+++ contract.js
@@ -1,4 +1,4 @@
-var password = prompt("সাইট এক্সেস করতে পাসওয়ার্ড দিন:");
+var password = prompt("সাইট এক্সেস যোগাযোগ করুন 01771687270:");
   if (password === "YOUR_PASSWORD") {
     document.body.style.display = 'block';
   } else {
